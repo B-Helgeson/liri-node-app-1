cc345dd6c3cae93b2683ff5df2146b0df5a972db
Marcela Errazquin <merrazquin@gmail.com>
2018-04-13 23:31:40 -0400

    Refactoring and documentation

--- liri.js
+++ liri.js
@@ -1,5 +1,16 @@
 require('dotenv').config()
 
+// consts
+const TWITTER_DATE_FORMAT = 'ddd MMM DD HH:mm:ss ZZ YYYY', // date provided in the format of Thu Apr 12 22:29:39 +0000 2018
+    OUTPUT_DATE_FORMAT = 'dddd, MMMM Do YYYY [at] h:mm A', // date displayed in the format of Thursday, April 12th 2018 at 6:29 PM
+    TWEET_PARAMS = { screen_name: 'bootcamp_marce', count: 20 },
+    DEFAULT_SONG = 'Ace of Base The Sign',
+    TRACK_FORMAT = '"%s" by %s on the album "%s"\n\n%s',
+    MOVIE_QUERY_FORMAT = 'http://www.omdbapi.com/?t=%s&y=&plot=short&apikey=trilogy',
+    DEFAULT_MOVIE = 'Mr. Nobody',
+    MOVIE_FORMAT = '"%s" (%s)\nIMDB Rating: %s\nTomatometer: %s\nCountry: %s\nLanguage: %s\n\n%s\n\nStarring: %s',
+    SEPARATOR = '**********'
+
 // modules
 const Twitter = require('twitter'),
     Spotify = require('node-spotify-api'),
@@ -17,18 +28,11 @@ var keys = require('./keys.js'),
 var userCommand = process.argv[2],
     userParameter = process.argv.slice(3).join(" ")
 
-// consts
-const TWITTER_DATE_FORMAT = 'ddd MMM DD HH:mm:ss ZZ YYYY', //date provided in the format of Thu Apr 12 22:29:39 +0000 2018
-    OUTPUT_DATE_FORMAT = 'dddd, MMMM Do YYYY [at] h:mm A',
-    DEFAULT_SONG = 'Ace of Base The Sign',
-    TRACK_FORMAT = '"%s" by %s on the album "%s"\n\n%s',
-    MOVIE_QUERY_FORMAT = 'http://www.omdbapi.com/?t=%s&y=&plot=short&apikey=trilogy',
-    DEFAULT_MOVIE = 'Mr. Nobody',
-    MOVIE_FORMAT = '"%s" (%s)\nIMDB Rating: %s\nTomatometer: %s\nCountry: %s\nLanguage: %s\n\n%s\n\nStarring: %s',
-    SEPARATOR = '**********'
-
-processCommand(userCommand, userParameter)
-
+/**
+ * Process command with optional parameter
+ * @param {string} command (required) Supported commands: my-tweets, spotify-this-song, movie-this, do-what-it-says
+ * @param {string} parameter (optional) 
+ */
 function processCommand(command, parameter) {
     switch (command) {
         case 'my-tweets':
@@ -48,8 +52,11 @@ function processCommand(command, parameter) {
     }
 }
 
+/**
+ * Displays the most recent 20 tweets
+ */
 function displayTweets() {
-    client.get('statuses/user_timeline', { screen_name: 'bootcamp_marce', count: 20 },
+    client.get('statuses/user_timeline', TWEET_PARAMS,
         function (error, tweets, response) {
             if (!error) {
                 tweets.forEach(tweet => {
@@ -63,11 +70,15 @@ function displayTweets() {
     )
 }
 
+/**
+ * Pull information on a Spotify track specified by songName
+ * @param {string} songName 
+ */
 function spotifySong(songName) {
     spotify.search({ type: 'track', query: songName, limit: 1 },
-        function (err, data) {
-            if (err) {
-                return console.log('Error occurred: ' + err)
+        function (error, data) {
+            if (error) {
+                return console.log('Error occurred: ' + error)
             }
 
             let track = data.tracks.items[0]
@@ -77,18 +88,22 @@ function spotifySong(songName) {
                 track.name,
                 track.artists.map(artist => artist.name).join(', '),
                 track.album.name,
-                track.external_urls.preview_url || ('No preview available; song URL: ' + track.external_urls.spotify))
+                track.external_urls.preview_url || ('No preview available. Full track URL: ' + track.external_urls.spotify))
             )
             console.log(SEPARATOR)
         }
     )
 }
 
+/**
+ * Pull information on a movie specified by movieName
+ * @param {string} movieName 
+ */
 function omdbMovie(movieName) {
     request(util.format(MOVIE_QUERY_FORMAT, movieName.replace(' ', '+')),
-        function (error, response, body) {
+        function (error, response, data) {
             if (!error && response.statusCode === 200) {
-                let movie = JSON.parse(body)
+                let movie = JSON.parse(data)
 
                 console.log(SEPARATOR)
                 console.log(util.format(MOVIE_FORMAT,
@@ -107,7 +122,14 @@ function omdbMovie(movieName) {
     )
 }
 
+/**
+ * Perform whichever task is specified in the random.txt file
+ */
 function performTaskFromFile() {
+    let fileCommand, fileParameter
     [fileCommand, fileParameter] = fs.readFileSync('random.txt', 'utf8').split(',')
     processCommand(fileCommand, fileParameter)
 }
+
+// process command specified by user input
+processCommand(userCommand, userParameter)
